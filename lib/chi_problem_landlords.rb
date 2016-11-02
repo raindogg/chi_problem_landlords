9940c3e27783cc41e615b8bdc381fda8446722b4
wabray17 <wabray17@gmail.com>
2016-11-01 19:46:07 -0500

    separate classes

--- lib/chi_problem_landlords.rb
+++ lib/chi_problem_landlords.rb
@@ -2,14 +2,7 @@ require "chi_problem_landlords/version"
 require "unirest"
 
 module ChiProblemLandlords
-  class Landlord
-    attr_accessor :community_area, :address, :respondent
-    def initialize(hash)
-      @community_area = hash["community_area"]
-      @address = hash["address"]
-      @respondent = hash["respondent"]
-    end
-
+  class Finder
     def all
       collection = []
       Unirest.get("https://data.cityofchicago.org/resource/dip3-ud6z.json").body.each do |landlord_hash|
@@ -22,6 +15,15 @@ module ChiProblemLandlords
     def find(params_id)
       landlord_array = Unirest.get("https://data.cityofchicago.org/resource/dip3-ud6z.json").body
       Landlord.new(landlord_array[params_id])
+    end
+  end
+
+  class Landlord
+    attr_accessor :community_area, :address, :respondent
+    def initialize(hash)
+      @community_area = hash["community_area"]
+      @address = hash["address"]
+      @respondent = hash["respondent"]
     end
   end
 end
